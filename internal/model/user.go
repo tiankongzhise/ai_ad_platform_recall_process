09a1ceefaecb077951f35f5d3ba8e893808d108d
hbc_thinkbook <419337244@qq.com>
2026-04-29 10:29:59 +0800

    fix(model): 修改用户注销唯一索引约束

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,14 +6,14 @@ import (
 
 type User struct {
 	ID                   uint64     `gorm:"primaryKey;autoIncrement" json:"-"`
-	UserName             string     `gorm:"column:user_name;type:varchar(64);not null;uniqueIndex:idx_user_name_logout,logout_at;comment:用户名" json:"user_name"`
+	UserName             string     `gorm:"column:user_name;type:varchar(64);not null;uniqueIndex:idx_user_name_logout;comment:用户名" json:"user_name"`
 	UID                  string     `gorm:"column:uid;type:varchar(64);uniqueIndex;comment:用户唯一标识UID" json:"uid"`
 	Phone                string     `gorm:"type:varchar(20);comment:手机号" json:"phone,omitempty"`
 	Password             string     `gorm:"type:varchar(255);not null;comment:加密密码" json:"-"`
 	ApiToken             string     `gorm:"type:varchar(64);uniqueIndex;comment:用户API Token(长期有效)" json:"api_token,omitempty"`
 	NotifyURL            string     `gorm:"type:varchar(512);default:'';comment:通知回调URL" json:"notify_url,omitempty"`
 	Status               int8       `gorm:"type:tinyint;default:1;comment:状态 1正常 0已注销" json:"status"`
-	LogoutAt             int64      `gorm:"column:logout_at;type:bigint;default:-1;comment:注销时间戳(秒)，-1表示活跃用户" json:"logout_at"`
+	LogoutAt             int64      `gorm:"column:logout_at;type:bigint;default:-1;uniqueIndex:idx_user_name_logout;comment:注销时间戳(秒)，-1表示活跃用户" json:"logout_at"`
 	CreatedAt            time.Time  `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt            time.Time  `gorm:"autoUpdateTime" json:"updated_at"`
 }
